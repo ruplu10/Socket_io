5ebc66d3761635adf646a25041c3e8dbaf30857e
ruplu10 <rupeshshahi594@gmail.com>
2023-05-28 03:03:29 +0545

    socket io used for very basic chat

---
+++ index.js
@@ -0,0 +1,27 @@
+const { Console, log } = require('console')
+const express = require('express')
+const app = express()
+const server = require('http').createServer(app)
+const io = require("socket.io") (server,{cors:{origin: "*"}})
+
+app.set('view engine', 'ejs')
+app.get('/' , (req,res) =>{
+
+    res.render('home')
+})
+
+server.listen(4000, () => {
+    console.log("server running");
+})
+
+
+io.on('connection',(socket)=>{
+    console.log("user connected..." + socket.id);
+
+
+
+
+    socket.on("message", (data) => {
+        socket.broadcast.emit('message', data)
+}) 
+})
